043f80b38904b4104ea96e7521751659242bd2ca
Raquel Justicia <raquel.justicia.p@gmail.com>
2013-09-05 19:49:17 +0100

    Add Solution file, FileManagerTests, Set restore packages on build

---
+++ Solutions/CSVWizard.IO.UnitTests/FileManagerTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using NUnit.Framework;
+
+namespace CSVWizard.IO.UnitTests
+{
+    public class FileManagerTests
+    {
+        [Test]
+        public void ShouldReturnNullIfFileDoesNotExist()
+        {
+            //Arrange
+            var fileManager = new FileManager();
+
+            //Act
+            var result = fileManager.ReadFile("fileName");
+
+            //Assert
+            Assert.That(result, Is.Null);
+        }
+    }
+
+    public class FileManager
+    {
+        public IEnumerable<string> ReadFile(string fileName)
+        {
+            if (!File.Exists(fileName))
+            {
+                return null;
+            }
+            throw new Exception();
+        }
+    }
+}
