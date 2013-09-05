f69d50392d35fbe64cf3eb6cddcb20df4a59c238
Raquel Justicia <raquel.justicia.p@gmail.com>
2013-09-05 20:58:49 +0100

    Copy test file to output and added read CSV test

--- Solutions/CSVWizard.IO.UnitTests/FileManagerTests.cs
+++ Solutions/CSVWizard.IO.UnitTests/FileManagerTests.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.IO;
 using NUnit.Framework;
+using System.Linq;
 
 namespace CSVWizard.IO.UnitTests
 {
     public class FileManagerTests
     {
+        private const string CSVFile = "TestFiles\\test.csv";
+
         [Test]
         public void ShouldReturnNullIfFileDoesNotExist()
         {
@@ -27,8 +30,25 @@ namespace CSVWizard.IO.UnitTests
             var fileManager = new FileManager();
 
             //Act && Assert
-            Assert.Throws<InvalidExtensionException>(() => fileManager.ReadFile("filename.a"));
-            
+            Assert.Throws<InvalidExtensionException>(() => fileManager.ReadFile("filename.a"));   
+        }
+
+        [Test]
+        public void ShouldReturnCSV()
+        {
+            //Arrange
+            const string csvLine1 = "a,b,c";
+            const string csvLine2 = "d,e,f";
+
+            var fileManager = new FileManager();
+
+            //Act
+            var result = fileManager.ReadFile(CSVFile);
+
+            //Assert
+            Assert.That(result.Count(), Is.EqualTo(2));
+            Assert.That(result.First(), Is.EqualTo(csvLine1));
+            Assert.That(result.Skip(1).First(), Is.EqualTo(csvLine2));
         }
     }
 
@@ -62,7 +82,7 @@ namespace CSVWizard.IO.UnitTests
                 return null;
             }
 
-            throw new Exception();
+            return File.ReadAllLines(fileName);
         }
     }
 }
